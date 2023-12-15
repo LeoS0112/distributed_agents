bbe7f400af953a6477a4b525f3daf5dfa6891216
Leo Sanitt <leo@sanitt.com>
2023-12-15 22:26:09 +0000

    adding message passing between BasicAgents

--- src/agents.rs
+++ src/agents.rs
@@ -1,4 +1,5 @@
 use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::thread::JoinHandle;
 use crate::raytracer::SceneObject;
@@ -12,22 +13,28 @@ pub trait Agent {
     fn distance_from(&self, point: &Vector) -> f64;
 }
 pub struct BasicAgent {
+    id: i64,
     body: Arc<Mutex<Box<dyn SceneObject + Send + Sync>>>,
+    sender: Mutex<Sender<String>>,
+    receiver: Mutex<Receiver<String>>,
 }
-
 impl BasicAgent {
-    pub fn new(body: Arc<Mutex<Box<dyn SceneObject + Send + Sync>>>) -> BasicAgent {
-        return BasicAgent { body }
+    pub fn new(id: i64, body: Arc<Mutex<Box<dyn SceneObject + Send + Sync>>>, sender: Mutex<Sender<String>>, receiver: Mutex<Receiver<String>>) -> BasicAgent {
+        return BasicAgent { id, body, sender, receiver }
     }
 }
 impl Agent for BasicAgent {
     fn act(&self, mut slf: Arc<Mutex<BasicAgent>>) -> JoinHandle<()>{
         let h = thread::spawn(move || {
-                let to_change = Vector::new(1.0, 1.0, 1.0);
-                let mut s = slf.lock().unwrap();
-                let new_location = &s.get_location().return_plus(&to_change);
-                s.set_location(new_location);
-
+            let to_change = Vector::new(1.0, 1.0, 1.0);
+            let mut slf_unlocked = slf.lock().unwrap();
+            let new_location = &slf_unlocked.get_location().return_plus(&to_change);
+            slf_unlocked.set_location(new_location);
+            let to_send = String::from(format!("Hello, I am: {}", slf_unlocked.id));
+            println!("{}", to_send);
+            slf_unlocked.sender.lock().unwrap().send(to_send).unwrap();
+            let received =  slf_unlocked.receiver.lock().unwrap().recv().unwrap();
+            println!("Thanks, I am {}", slf_unlocked.id);
         });
         return h;
     }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::process::Command;
-use std::thread;
-use std::sync::mpsc;
+use std::sync::mpsc::{Sender, Receiver, channel};
 use std::sync::{Arc, Mutex};
 use show_image::{ImageView, ImageInfo, create_window};
+use show_image::event::VirtualKeyCode::Mute;
 use summer2023::matrices::{Vector};
 use summer2023::raytracer::scene::{Camera, Contents, draw, LightSource, Screen};
 use summer2023::raytracer::{Colour, scene_objects::Sphere, SceneObject};
 use summer2023::agents::{Agent, BasicAgent};
+
 #[show_image::main]
 fn main() {
-
     // Creating the test spheres
     let mut test_sphere = Sphere { radius: 150.0, location: Vector::new(0.0, 0.0, 1200.0) };
     let mut test_sphere2 = Sphere { radius: 120.0, location: Vector::new(150.0, 150.0, 1400.0) };
@@ -35,9 +35,13 @@ fn main() {
         intensity: 0,
     };
 
+    let mut r_channels: Vec<Receiver<String>>= vec![];
+    let mut s_channels: Vec<Sender<String>> = vec![];
+    let (sa, ra) = channel();
+    let (sb, rb) = channel();
     // Creating the agents
-    let mut a = Arc::new(Mutex::new(BasicAgent::new(Arc::new(Mutex::new(Box::new(test_sphere))))));
-    let mut b = Arc::new(Mutex::new(BasicAgent::new(Arc::new(Mutex::new(Box::new(test_sphere2))))));
+    let mut a = Arc::new(Mutex::new(BasicAgent::new(0, Arc::new(Mutex::new(Box::new(test_sphere))), Mutex::new(sa), Mutex::new(rb))));
+    let mut b = Arc::new(Mutex::new(BasicAgent::new(1, Arc::new(Mutex::new(Box::new(test_sphere2))), Mutex::new(sb), Mutex::new(ra))));
 
 
     // Running the simulation
@@ -45,10 +49,9 @@ fn main() {
     let mut to_show = Vec::new();
     let mut agents: Vec<Arc<Mutex<BasicAgent>>> = vec![a.clone(), b.clone()];
 
-    for n in 0..100 {
+    for n in 0..10 {
         let mut handles = vec![];
         let num_agents = agents.len();
-
         for agent_num in 0..num_agents {
             handles.push(agents[agent_num].lock().unwrap().act(agents[agent_num].clone()));
         }
@@ -64,16 +67,11 @@ fn main() {
         let contents = Contents { objects: scene_objects, light: vec![&test_light, &other_test_light] };
         let pixel_data = draw(&test_cam, &test_screen, &contents);
         to_show.push(pixel_data);
-
-        for i in 0..num_agents {
-
-        }
-
     }
 
     // Playing the simulation in a loop
     loop {
-        for pd in 0..100{
+        for pd in 0..10{
             let image = ImageView::new(ImageInfo::rgb8((2 * test_screen.width) as u32, (2 * test_screen.height) as u32), &to_show[pd]);
             window.set_image("image-001", image).expect("set image");
             let mut child = Command::new("sleep").arg("0.02").spawn().unwrap();
